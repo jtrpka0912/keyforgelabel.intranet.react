d56b51ad599c8593504411ac5749bd4dfee3fc33
jtrpka0912 <jtrpka0912@gmail.com>
2024-08-19 15:32:02 +0000

    Created the layout for the label and placeholder 'houses'

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -9,8 +9,16 @@ import './label.css';
 const Label = () => {
     return (
         <div className="label">
-            <h2 className="label__name">Jeremy of the Flibbles</h2>
-            <p className="label__set">Grim Reminders</p>
+            <div className="label__left">
+                <h2 className="label__name">Jeremy of the grand majestic Flibbles</h2>
+                <p className="label__set">Grim Reminders</p>
+            </div>
+
+            <div className="label__right">
+                <p>Hi</p>
+                <p>Hi</p>
+                <p>Hi</p>
+            </div>
         </div>
     );
 };

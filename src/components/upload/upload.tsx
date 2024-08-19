bfebbc708435a11c89e9307a6382ffd3911040f7
jtrpka0912 <jtrpka0912@gmail.com>
2024-08-19 17:25:57 +0000

    Finally using some form of TypeScript.

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,23 +1,70 @@
+import React from 'react';
 import './upload.css';
+import { UploadButtonProps, UploadDialogProps } from './upload.types';
+
+/**
+ * @function UploadDialog
+ * @description The dialog where the user can upload a deck to the sheet
+ * @author J. Trpka
+ * @param {boolean} isOpen
+ * @returns {JSX.Element}
+ */
+const UploadDialog = ({isOpen}: UploadDialogProps) => {
+    const elementRef = React.useRef<HTMLDialogElement>(null);
+
+    React.useEffect(() => {
+        if(elementRef && elementRef.current) {
+            if(isOpen) {
+                elementRef.current.showModal();
+            } else {
+                elementRef.current.close();
+            }
+            
+        }
+    }, [isOpen]);
+
+    return (
+        <dialog 
+            ref={elementRef} 
+            className="upload__dialog">
+            <p>Hello World</p>
+        </dialog>
+    );
+};
 
 /**
  * @function UploadButton
  * @description The floating action button to bring up the upload pop-up
  * @author J. Trpka
- * @returns 
+ * @param {function} onClick
+ * @returns {JSX.Element}
  */
-const UploadButton = () => {
+const UploadButton = ({onClick}: UploadButtonProps) => {
     return (
-        <button
+        <button 
             className="upload__button"
+            onClick={onClick}
         >Upload</button>
     );
 };
 
+/**
+ * @function Upload
+ * @description The wrapper that contains the FAB and Dialog
+ * @author J. Trpka
+ * @returns {JSX.Element}
+ */
 const Upload = () => {
+    const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+    const handleOnClickOpenModal = () => {
+        setIsOpen(true);
+    }
+
     return (
-        <div>
-            <UploadButton />
+        <div className="upload">
+            <UploadButton onClick={handleOnClickOpenModal} />
+            <UploadDialog isOpen={isOpen} />
         </div>
     );
 };
---
+++ src/components/upload/upload.types.ts
@@ -0,0 +1,7 @@
+export type UploadDialogProps = {
+    isOpen: boolean;
+}
+
+export type UploadButtonProps = {
+    onClick: () => void;
+};

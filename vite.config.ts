92e086f34bd202329e1a427da84d04ced92bf964
jtrpka0912 <jtrpka0912@gmail.com>
2024-08-19 14:20:33 +0000

    Cleaned up app. Got it setup for IDX environment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, ConfigEnv, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+
+//   plugins: [react()],
+// });
+
+export default defineConfig(({command, mode} : ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.PORT),
+    }
+  }
+});

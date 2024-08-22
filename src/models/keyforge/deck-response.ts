6ec08ea273d339018ae5719b1e27178778cde783
jtrpka0912 <jtrpka0912@gmail.com>
2024-08-22 12:20:24 +0000

    Making a deck class.

--- src/models/keyforge/deck-response.ts
+++ src/models/keyforge/deck-response.ts
@@ -1,17 +1,4 @@
-export enum Expansions {
-    CallOfTheArchons = 341,
-    AgeOfAscension = 435,
-    WorldsCollide = 452,
-    MassMutations = 479,
-    DarkTidings = 496,
-    WindsOfExchange = 600,
-    Unchained2022 = 601,
-    VaultMasters2023 = 609,
-    GrimReminders = 700,
-    Menagerie2024 = 722,
-    VaultMasters2024 = 737,
-    // TODO: Add the new ones
-}
+
 
 export type DeckResponse = {
     data: DeckData;
